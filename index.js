cc4ce1d359b273fcf857c71fb77820fc5e86ac00
vikram singh panwar <vikramsinghpanwar000@gmai.com>
2023-11-09 22:45:54 +0530

    update121

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ require('dotenv').config();
 const DBURL = "mongodb+srv://WarPanGames:zJyHZPvwMFxdf4qS@cluster0.dziwy2k.mongodb.net/WarPanGames?retryWrites=true&w=majority";
 // process.env.dbURL;
 App.use(express.static(path.join(__dirname, "public")));
-App.use(express.static('F:/Warpan/Website/WebApp/WarpanGames/Unity')); // Replace with the actual path to your external folder
-
+App.use(express.static('/var/www/unity')); // Replace with the actual path to your external folder
+//F:/Warpan/Website/WebApp/WarpanGames/Unity
 App.use('/',router.ProjectRouter);
 App.use(express.json());
 App.use(bodyparser.urlencoded({extended: 'true'}));
